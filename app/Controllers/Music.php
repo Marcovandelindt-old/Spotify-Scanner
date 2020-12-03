3200e00267b99ea53ab04a03be678e66bd09a590
Marco van de Lindt <marcovandelindt@live.nl>
2020-12-03 11:49:38 +0100

    Correctly authorize and update code if needed

--- app/Controllers/Music.php
+++ app/Controllers/Music.php
@@ -23,37 +23,61 @@ class Music extends BaseController
 	 */
 	public function authorize ()
 	{
-		$options = [
-			'scope' => [
-				'ugc-image-upload',
-				'user-read-playback-state',
-				'user-modify-playback-state',
-				'user-read-currently-playing',
-				'streaming',
-				'app-remote-control',
-				'user-read-email',
-				'user-read-private',
-				'playlist-read-collaborative',
-				'playlist-modify-public',
-				'playlist-read-private',
-				'playlist-modify-private',
-				'user-library-modify',
-				'user-library-read',
-				'user-top-read',
-				'user-read-playback-position',
-				'user-read-recently-played',
-				'user-follow-read',
-				'user-follow-modify',
-			],
-		];
-
-		$settingModel = new \App\Models\Setting;
+		# Set new Spotify Session
+		$settingModel 	= new \App\Models\Setting();
 		$spotifySession = new SpotifySession(
 			$settingModel->getByName('spotifyClientId')->value,
 			$settingModel->getByName('spotifyClientSecret')->value,
 			$settingModel->getByName('spotifyRedirectURI')->value
 		);
 
+		# Set scopes for authorizing the user
+		$scopes = [
+			'scope' => [
+				'ugc-image-upload',
+				'user-read-recently-played',
+				'user-top-read',
+				'user-read-playback-position',
+				'user-read-playback-state',
+				'user-read-currently-playing',
+				'app-remote-control',
+				'streaming',
+				'playlist-modify-public',
+				'playlist-modify-private',
+				'playlist-read-collaborative',
+				'user-follow-modify',
+				'user-follow-read',
+				'user-library-modify',
+				'user-library-read',
+				'user-read-email',
+				'user-read-private',
+			]
+		];
+
+		# Redirect to authorization url
 		return redirect()->to($spotifySession->getAuthorizeUrl($options));
+	}
+
+	/**
+	 * Set new Spotify Code
+	 */
+	public function setCode ()
+	{
+		if (!empty($this->request->getGet('code'))) {
+			$code 		  = $this->request->getGet('code');
+			$settingModel = new \App\Models\Setting();
+			$existingCode = $settingModel->getByName('spotifyCode')->value;
+
+			if ($existingCode != $code) {
+				$settingModel->update(
+					$settingModel->getByName('spotifyCode')->setting_id,
+					[
+						'value' => $code,
+					]
+				);
+			}
+		}
+
+		return redirect()->to('/');
 	}
 }
